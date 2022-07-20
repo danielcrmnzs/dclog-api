c07aa5f4cf0515c26be3d67d5f3b0e4b86a951f8
Daniel Coimbra <danielcrmnzs@hotmail.com>
2022-07-20 18:38:05 -0300

    Projeto criado

---
+++ src/main/java/com/devcoi/dclog/DclogApiApplication.java
@@ -0,0 +1,13 @@
+package com.devcoi.dclog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DclogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DclogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devcoi/dclog/DclogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcoi.dclog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DclogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
