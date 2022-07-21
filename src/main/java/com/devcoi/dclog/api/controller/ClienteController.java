21a69b4845205a193ccc700cfbe70e28b951e033
Daniel Coimbra <danielcrmnzs@hotmail.com>
2022-07-21 08:17:16 -0300

    Configurando JPA e adicionando a entidade Cliente

--- src/main/java/com/devcoi/dclog/api/controller/ClienteController.java
+++ src/main/java/com/devcoi/dclog/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.devcoi.dclog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +13,12 @@ import com.devcoi.dclog.domain.model.Cliente;
 @RestController
 public class ClienteController {
 
+	@PersistenceContext
+	private EntityManager em;
+
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente(1L, "João da Silva", "joao@devcoi.com", "21 999-9999");
-		var cliente2 = new Cliente(2L, "Maria Rita", "maria@devcoi.com", "31 988-9999");
-
-		return Arrays.asList(cliente1, cliente2);
+		return em.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 
 }
--- src/main/java/com/devcoi/dclog/domain/model/Cliente.java
+++ src/main/java/com/devcoi/dclog/domain/model/Cliente.java
@@ -1,5 +1,11 @@
 package com.devcoi.dclog.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -11,15 +17,19 @@ import lombok.Setter;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Cliente {
 
 	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String nome;
-	
+
 	private String email;
-	
+
+	@Column(name = "telefone")
 	private String telefone;
 
 }
