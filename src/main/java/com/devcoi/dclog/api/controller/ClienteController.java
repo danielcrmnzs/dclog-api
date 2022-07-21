f9c0836f8e85c9e1640481fe841b02f58abfc169
Daniel Coimbra <danielcrmnzs@hotmail.com>
2022-07-21 08:26:01 -0300

    Configurando Spring Data JPA e criando recurso listar todos clientes

--- src/main/java/com/devcoi/dclog/api/controller/ClienteController.java
+++ src/main/java/com/devcoi/dclog/api/controller/ClienteController.java
@@ -2,23 +2,22 @@ package com.devcoi.dclog.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devcoi.dclog.domain.model.Cliente;
+import com.devcoi.dclog.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
 
-	@PersistenceContext
-	private EntityManager em;
-
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		return em.createQuery("from Cliente", Cliente.class).getResultList();
+		return clienteRepository.findAll();
 	}
 
 }
