9c604ff6ca85de09e72544cfdffaf2969ac366d4
Daniel Coimbra <danielcrmnzs@hotmail.com>
2022-07-21 23:55:49 -0300

    Adicionado recurso para salvar cliente

--- src/main/java/com/devcoi/dclog/api/controller/ClienteController.java
+++ src/main/java/com/devcoi/dclog/api/controller/ClienteController.java
@@ -3,10 +3,14 @@ package com.devcoi.dclog.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devcoi.dclog.domain.model.Cliente;
@@ -38,6 +42,12 @@ public class ClienteController {
 		 * if (optional.isPresent()) { return ResponseEntity.ok(optional.get()); } else
 		 * { return ResponseEntity.notFound().build(); }
 		 */
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar (@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
 	}
 
 }
