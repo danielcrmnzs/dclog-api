507ecf4a13c25f36f458075e399005f1bf374d58
Daniel Coimbra <danielcrmnzs@hotmail.com>
2022-07-22 00:22:18 -0300

    Adicionado recurso de atualizar Cliente

--- src/main/java/com/devcoi/dclog/api/controller/ClienteController.java
+++ src/main/java/com/devcoi/dclog/api/controller/ClienteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,6 +60,16 @@ public class ClienteController {
 			cliente = clienteRepository.save(cliente);
 
 			return ResponseEntity.ok(cliente);
+		}
+	}
+
+	@DeleteMapping("/{clienteId}")
+	public ResponseEntity<Void> apagar(@PathVariable Long clienteId) {
+		if (!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		} else {
+			clienteRepository.deleteById(clienteId);
+			return ResponseEntity.noContent().build();
 		}
 	}
 
