b51cbd8a4689d1d45ee6d84712f41f4ce2d0a44e
Daniel Coimbra <danielcrmnzs@hotmail.com>
2022-07-21 23:37:29 -0300

    Adicionado recurso buscar cliente por id

--- src/main/java/com/devcoi/dclog/api/controller/ClienteController.java
+++ src/main/java/com/devcoi/dclog/api/controller/ClienteController.java
@@ -3,33 +3,41 @@ package com.devcoi.dclog.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devcoi.dclog.domain.model.Cliente;
 import com.devcoi.dclog.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
-	@GetMapping("/clientes")
+
+	@GetMapping
 	public List<Cliente> listar() {
 		return clienteRepository.findAll();
 	}
-	
-	@GetMapping("/clientes/nomeExato")
-	public List<Cliente> buscarPorNome() {
-		return clienteRepository.findByNome("João da Silva");
-	}
 
-	@GetMapping("/clientes/nomeContendo")
-	public List<Cliente> buscarPorNomeContendo() {
-		return clienteRepository.findByNomeContaining("ria");
-	}
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		return clienteRepository.findById(clienteId)
+				// .map(cliente -> ResponseEntity.ok(cliente))
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
 
-	
+		/*
+		 * Optional<Cliente> optional = clienteRepository.findById(clienteId);
+		 * 
+		 * 
+		 * if (optional.isPresent()) { return ResponseEntity.ok(optional.get()); } else
+		 * { return ResponseEntity.notFound().build(); }
+		 */
+	}
 
 }
