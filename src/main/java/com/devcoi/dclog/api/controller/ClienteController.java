d3e642b96d54da6aa13a5d50a99c3661a56502b6
Daniel Coimbra <danielcrmnzs@hotmail.com>
2022-07-22 00:13:06 -0300

    Adicionado recurso de atualização de Cliente

--- src/main/java/com/devcoi/dclog/api/controller/ClienteController.java
+++ src/main/java/com/devcoi/dclog/api/controller/ClienteController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -32,8 +33,7 @@ public class ClienteController {
 	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
 		return clienteRepository.findById(clienteId)
 				// .map(cliente -> ResponseEntity.ok(cliente))
-				.map(ResponseEntity::ok)
-				.orElse(ResponseEntity.notFound().build());
+				.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
 
 		/*
 		 * Optional<Cliente> optional = clienteRepository.findById(clienteId);
@@ -43,11 +43,23 @@ public class ClienteController {
 		 * { return ResponseEntity.notFound().build(); }
 		 */
 	}
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente adicionar (@RequestBody Cliente cliente) {
+	public Cliente adicionar(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
+	}
+
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, @RequestBody Cliente cliente) {
+		if (!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		} else {
+			cliente.setId(clienteId);
+			cliente = clienteRepository.save(cliente);
+
+			return ResponseEntity.ok(cliente);
+		}
 	}
 
 }
